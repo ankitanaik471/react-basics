525fc9a66eb07934668448f4de34931d527fce41
Ankita Naik <ankitanaik471@gmail.com>
2022-09-22 20:59:03 +0530

    Adding image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import logoImage from './Images/logo.svg';
 
 function App() {
   return (
-  <div>
+  <div className='App'>
     <h1>Hello, This is my Application</h1>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+    <img src={logoImage} alt="image" className="App-logo"></img>
   </div>
   )
 }
