59478440d04d23c1bedddcd027af58aa791466de
Ankita Naik <ankitanaik471@gmail.com>
2022-09-27 22:53:38 +0530

    small fixes

--- src/components/skills/skill/Skill.js
+++ src/components/skills/skill/Skill.js
@@ -5,7 +5,7 @@ import './Skill.css';
 
 const Skill = (props) => {
     
-    return <div className="p-3 card text-center w-100 me-2">{ props.children }</div> ;
+    return <div className="p-3 card text-center w-100 me-sm-2 mb-2 mb-sm-0">{ props.children }</div> ;
 }
 
 export default Skill;
